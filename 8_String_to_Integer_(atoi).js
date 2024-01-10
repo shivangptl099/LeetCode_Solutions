6e7f26a538430063bdc8881dabd5223e00dce48c
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-10 20:09:30 +0530

    Create 8_String_to_Integer_(atoi).js

---
+++ 8_String_to_Integer_(atoi).js
@@ -0,0 +1,23 @@
+var myAtoi = function(s) {
+    var n = s.length;
+    var myString = "";
+    let flag = 0;
+    let i = 0;
+    while(i<n){
+        if(s[i].charCodeAt(0) === 43 || s[i].charCodeAt(0) === 45){
+            myString = myString + s[i];
+            flag = 1;
+        }
+        if(s[i].charCodeAt(0) >= 49 && s[i].charCodeAt(0) <= 57){
+            myString = myString + s[i];
+            flag = 1;
+        }
+        else{
+            if(flag === 1){
+                break;
+            }
+        }
+        i++;
+    }
+    return parseInt(myString);
+};
