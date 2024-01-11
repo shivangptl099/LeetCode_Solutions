e42af941156ebd17447ae34328791f6a9324f890
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-11 22:28:03 +0530

    Update 125_Valid_Palindrome.js

--- 125_Valid_Palindrome.js
+++ 125_Valid_Palindrome.js
@@ -2,7 +2,10 @@ var isPalindrome = function(s) {
     var i = 0;
     var myString = new Array();
     while(i < s.length){
-        if(s[i].charCodeAt(0) >= 65 && s[i].charCodeAt(0) <= 90){
+        if(s[i].charCodeAt(0) >= 48 && s[i].charCodeAt(0) <= 57){
+            myString.push(s[i]);
+        }
+        else if(s[i].charCodeAt(0) >= 65 && s[i].charCodeAt(0) <= 90){
             myString.push(s[i].toLowerCase());
         }
         else if(s[i].charCodeAt(0) >= 97 && s[i].charCodeAt(0) <= 122){
