e19ad82174b0da14e5e6070f6d5a6263edcedccf
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-14 15:55:51 +0530

    Update 43_Multiply_Strings.js

--- 43_Multiply_Strings.js
+++ 43_Multiply_Strings.js
@@ -1,5 +1,5 @@
 var multiply = function(num1, num2) {
-    let n1 = parseInt(num1);
-    let n2 = parseInt(num2);
+    let n1 = BigInt(num1);
+    let n2 = BigInt(num2);
     return (n1*n2).toString();
 };
