f6aab8bd63286b1cbb6b6c41ea1a50afd673a663
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-14 12:48:11 +0530

    Update 415_Add_Strings.js

--- 415_Add_Strings.js
+++ 415_Add_Strings.js
@@ -1,7 +1,6 @@
 var addStrings = function(num1, num2) {
-    let n1 = parseInt(num1);
-    let n2 = parseInt(num2);
-    if(n1.length)
-    let sum = n1 + n2;
+    let n1 = BigInt(num1);
+    let n2 = BigInt(num2);
+    let sum = (n1+n2);
     return sum.toString();
 };
