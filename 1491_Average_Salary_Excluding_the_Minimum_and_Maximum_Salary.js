8dbb1d1e7607f7a8678408cdb114efe59f8e8fe9
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-16 22:45:24 +0530

    Create 1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.js

---
+++ 1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.js
@@ -0,0 +1,7 @@
+var average = function(salary) {
+    salary = salary.sort((a,b) => (a-b));
+    let sum = 0;
+    for(let i = 1; i < salary.length - 1; i++)
+    sum += salary[i];
+    return sum/(salary.length - 2);
+};
