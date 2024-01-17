02e6147d0b61fe01c7444573bbd16ab7ca7d739c
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-17 20:17:17 +0530

    Create 1550_Three_Consecutive_Odds.js

---
+++ 1550_Three_Consecutive_Odds.js
@@ -0,0 +1,19 @@
+var threeConsecutiveOdds = function(arr) {
+    let i = 0;
+    while(i<arr.length){
+        let count = 0;
+        if(arr[i] % 2 !== 0){
+            let j = i+1;
+            while(count<3){
+                if(arr[j] % 2 === 0) break;
+                else{
+                    count++;
+                }
+                j++;
+            }
+            if(count === 2) return true;
+        }
+        i++;
+    }
+    return false;
+};
