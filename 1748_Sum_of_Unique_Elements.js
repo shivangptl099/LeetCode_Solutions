52fd0f60053127a670e79b3d3797cab1158726b0
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-19 13:48:39 +0530

    Update 1748_Sum_of_Unique_Elements.js

--- 1748_Sum_of_Unique_Elements.js
+++ 1748_Sum_of_Unique_Elements.js
@@ -1,11 +1,11 @@
 var sumOfUnique = function(nums) {
     nums = nums.sort((a,b) => a-b);
     let i = 0;
-    let last = 0;
     let sum = 0;
     while(i<nums.length){
-        if(nums[i] !== last) sum += nums[i];
-        last = nums[i];
+        if(i === 0 && nums[i] !== nums[i+1]) sum += nums[i];
+        else if(i === nums.length - 1 && nums[i-1] !== nums[i]) sum += nums[i];
+        else if(nums[i] !== nums[i-1] && nums[i] !== nums[i+1]) sum += nums[i];
         i++;
     }
     return sum;
