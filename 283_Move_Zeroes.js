d4a631748d67751381f5c1534d7ec6885f95e02a
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-19 13:23:16 +0530

    Update 283_Move_Zeroes.js

--- 283_Move_Zeroes.js
+++ 283_Move_Zeroes.js
@@ -1,17 +1,12 @@
-var moveZeroes = function(nums) {
-    nums = nums.sort((a,b) => b-a);
-    let i = nums.length - 1;
-    let count = 0;
-    while(i>=0){
-        if(nums[i] !== 0)break;
-        else{
-            count++;
-            nums.pop();
-        }
-        i--;
+nums = nums.sort((a,b) => a-b);
+    let i = 0;
+    let c = 0;
+    while(i<nums.length){
+        if(nums[i] === 0) c++;
+        else break;
+        i++;
     }
-    nums.reverse();
-    for(i = 0; i<count; i++)
+    nums = nums.filter(x => x!== 0);
+    for(i = 0;i<c;i++)
     nums.push(0);
     return nums;
-};
