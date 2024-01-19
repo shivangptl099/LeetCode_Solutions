8cc15bc25b147940a01da265cee7119ab5894b9d
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-19 22:17:49 +0530

    Update 67_Add_Binary.js

--- 67_Add_Binary.js
+++ 67_Add_Binary.js
@@ -1,6 +1,3 @@
 var addBinary = function(a, b) {
-    let n1 = parseInt(a,2);
-    let n2 = parseInt(b,2);
-    let sum = n1 + n2;
-    return sum.toString(2);
+    return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
 };
