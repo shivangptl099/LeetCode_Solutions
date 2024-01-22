c9f3975f9d4d4822092ffdf8bcbe40eb975d901d
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-22 22:30:09 +0530

    Update 66_Plus_One.js

--- 66_Plus_One.js
+++ 66_Plus_One.js
@@ -1,26 +1,9 @@
 var plusOne = function(digits) {
-    var i = digits.length - 1;
-    var sum = 0;
-    while(i >= 0){
-        if(i === digits.length){
-            sum = sum + digits[i];
-        }
-        else{
-            let j = digits.length - 1;
-            var f = 1;
-            while(j>i){
-                f *= 10;
-                j--;
-            }
-            sum += f*digits[i];  
-        }
-        i--;
-    }
-    sum++;
-    var arr = new Array();
-    var s = sum.toString();
-    for(i = 0; i < s.length; i++){
-        arr.push(parseInt(s[i]));
-    }
-    return arr;
+    let s = digits.join("");
+    s = BigInt(s) + 1n;
+    s = s.toString();
+    digits = [];
+    for(let i = 0; i < s.length; i++)
+    digits.push(parseInt(s[i]));
+    return digits;
 };
