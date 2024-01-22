dbf070f784580886c9d932aa59186cc33fde1ab5
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-22 19:45:17 +0530

    Create 344_Reverse_String.js

---
+++ 344_Reverse_String.js
@@ -0,0 +1,14 @@
+var reverseString = function(s) {
+    let i = 0;
+    let j = s.length - 1;
+    while(i<j){
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+    return s;
+};
+
+// We can solve it using s.reverse();
