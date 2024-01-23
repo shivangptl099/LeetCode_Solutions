58a471b9811e00215c6afa7c7ec553755535c7e8
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-23 17:59:07 +0530

    Create 26_Remove_Duplicates_from_Sorted_Array.js

---
+++ 26_Remove_Duplicates_from_Sorted_Array.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(nums) {
+    for(let i = 0; i < nums.length - 1; i++){
+        if(nums[i] === nums[i + 1]){
+            nums.splice(i+1,1);
+            i--;
+        }
+    }
+    return nums.length;
+};
