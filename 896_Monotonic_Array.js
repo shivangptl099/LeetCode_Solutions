0bdf566f451e1565fca31a20367607a38c18a113
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-24 13:47:08 +0530

    Update 896_Monotonic_Array.js

--- 896_Monotonic_Array.js
+++ 896_Monotonic_Array.js
@@ -1,16 +1,10 @@
 var isMonotonic = function(nums) {
-    if(nums[0] <= nums[1]){
-        let i = 1;
-        for(i; i < nums.length - 1; i++){
-            if(nums[i] > nums[i+1]) return false;
-        }
-        if(i === nums.length - 1) return true;
+    let increase = false;
+    let decrease = false;
+    for(let i = 0; i < nums.length - 1; i++){
+        if(nums[i] < nums[i + 1]) increase = true;
+        else if(nums[i] > nums[i + 1]) decrease = true;
+        if(increase && decrease) return false;
     }
-    else if(nums[0] >= nums[1]){
-        let i = 1;
-        for(i; i < nums.length - 1; i++){
-            if(nums[i] < nums[i+1]) return false;
-        }
-        if(i === nums.length - 1) return true;
-    }
+    return true;
 };
