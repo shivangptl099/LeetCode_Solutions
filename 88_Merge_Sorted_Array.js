d0729c812bbb6a0e4c3d786244c65a01a612f89f
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-24 22:39:14 +0530

    Update 88_Merge_Sorted_Array.js

--- 88_Merge_Sorted_Array.js
+++ 88_Merge_Sorted_Array.js
@@ -1,9 +1,8 @@
 var merge = function(nums1, m, nums2, n) {
-    nums1 = nums1.filter(x => x!== 0);
-    let i = 0;
-    while(i<nums2.length){
-        nums1.push(nums2[i]);
-        i++;
+    let j = 0;
+    for(let i = m; i < m + n; i++){
+    nums1[i] = nums2[j];
+    j++;
     }
-    return nums1.sort();
+    return nums1.sort((a,b) => a-b);
 };
