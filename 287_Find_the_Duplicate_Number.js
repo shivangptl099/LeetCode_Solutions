9665613545c197ef8e053e7db444425d0817df34
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-26 23:41:25 +0530

    Update 287_Find_the_Duplicate_Number.js

--- 287_Find_the_Duplicate_Number.js
+++ 287_Find_the_Duplicate_Number.js
@@ -1,6 +1,7 @@
-287. Find the Duplicate Numbervar findDuplicate = function(nums) {
-    for(let i = 0; i < nums.length - 1; i++){
-        for(let j = i + 1; j < nums.length; j++)
-        if(nums[i] === nums[j]) return nums[i];
+var findDuplicate = function(nums) {
+    let map1 = new Map();
+    for(let i = 0; i < nums.length; i++){
+        if(map1.has(nums[i])) return nums[i];
+        else map1.set(nums[i],1);
     }
 };
