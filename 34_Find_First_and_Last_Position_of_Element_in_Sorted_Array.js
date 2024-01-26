b60d3ba18cc45cadc6923d59596ac76dcaec7e66
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-26 11:43:16 +0530

    Update 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js

--- 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -1,21 +1,9 @@
-let low = 0;
-    let high = nums.length - 1;
+var searchRange = function(nums, target) {
+    let s = nums.indexOf(target);
     let arr = [];
-    while(low <= high){
-        let mid = Math.floor((low + high)/2);
-        if(nums[mid] === target){
-            if(nums[mid - 1] === target) arr[0] = mid - 1;
-            else arr[0] = mid;
-            let j = mid + 1;
-            while(j < nums.length && nums[j] === target){
-                j++;
-            }
-            if(j - 1 >= 0 && nums[j - 1] === target) arr[1] = j - 1;
-            else arr[1] = arr[0];
-            break;
-        }
-        else if(nums[mid] < target) low = mid + 1;
-        else high = mid - 1;
-    }
-    if(arr.length === 0) return [-1,-1];
+    arr[0] = s;
+    while(s < nums.length && nums[s] === target) s++;
+    if(nums[s-1] === target) arr[1] = s-1;
+    else arr[1] = arr[0];
     return arr;
+};
