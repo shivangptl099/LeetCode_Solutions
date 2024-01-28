10a58c5adbd31daf38455d273af4360a6c4b2f5e
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-28 23:28:53 +0530

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.js

--- 448_Find_All_Numbers_Disappeared_in_an_Array.js
+++ 448_Find_All_Numbers_Disappeared_in_an_Array.js
@@ -1,14 +1,14 @@
 var findDisappearedNumbers = function(nums) {
-    let max = nums.length;
-    var arr = [];
-    let i = 0;
-    for(i;i < max; i++){
-        arr[i] = i+1;
+    let arr = [];
+    let n = nums.length;
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(!map.has(nums[i])) map.set(nums[i],1);
     }
-    i = 0;
-    while(i < nums.length){
-        arr = arr.filter(x => x !== nums[i]);
-        i++;
+    for(let i = 1; i <= n; i++)
+    {
+        if(!map.has(i)) arr.push(i);
     }
     return arr;
 };
