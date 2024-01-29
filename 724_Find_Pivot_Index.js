39023bd48e3b7ae4aceb0d17ca21d0eec76d7a61
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-01-29 13:40:50 +0530

    Update 724_Find_Pivot_Index.js

--- 724_Find_Pivot_Index.js
+++ 724_Find_Pivot_Index.js
@@ -1,19 +1,17 @@
 var pivotIndex = function(nums) {
-    let leftSum = 0;
     let rightSum = 0;
+    for(let i = 0; i < nums.length; i++) rightSum += nums[i];
+    let leftSum = 0;
     let ans = -1;
-    for(let i = 0; i < nums.length ; i++)
+    for(let i = 0; i < nums.length; i++)
     {
-        rightSum = 0;
-        for(let j = i + 1; j < nums.length; j++)
-        {
-            rightSum += nums[j];
-        }
+        let temp = nums[i]
+        rightSum -= temp;
         if(leftSum == rightSum)
         {
             return i;
         }
-        leftSum += nums[i];
+        leftSum += temp;
     }
     return ans;
 };
