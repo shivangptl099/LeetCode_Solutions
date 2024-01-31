07b35a32d7f85adc40324964c3eb85d26d86ffd3
shivangptl099 <117822313+shivangptl099@users.noreply.github.com>
2024-02-01 00:32:28 +0530

    Update 917_Reverse_Only_Letters.js

--- 917_Reverse_Only_Letters.js
+++ 917_Reverse_Only_Letters.js
@@ -1,21 +1,52 @@
+// Method 1 : Using Brute Force With using An array and for loops
+function isLetter(char){
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
 var reverseOnlyLetters = function(s) {
     s = s.split("");
     let arr = [];
     for(let i = 0; i < s.length; i++){
-        if(s[i].charCodeAt(0) >=97 && s[i].charCodeAt(0) <= 122){
-            arr.push(s[i]);
-            s[i] = -1;
-        }
-        else if(s[i].charCodeAt(0) >=65 && s[i].charCodeAt(0) <= 90){
+        if(isLetter(s[i])){
             arr.push(s[i]);
             s[i] = -1;
         }
     }
     for(let i = 0; i < s.length; i++){
         if(s[i] == -1){
-            let t = arr.pop();
-            s[i] = t;
+            s[i] = arr.pop();
         }
     }
     return s.join("");
 };
+// Runtime : 47ms 
+// 88.67% Users Beat
+
+//Method 2: Using The Two Pointer Method 
+function isLetter(char){
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
+var reverseOnlyLetters = function(s) {
+    let left = 0
+    let right = s.length - 1
+    s = s.split("")
+    while(left < right){
+        while(left < right && !isLetter(s[left])){
+            left++
+        }
+        while(left < right && !isLetter(s[right])){
+            right--
+        }
+
+        let temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+
+        left++
+        right--
+    }
+    return s.join("")
+};
+// Runtime : 50ms
+// 74% Users Beat
