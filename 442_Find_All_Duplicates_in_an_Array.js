d74bd0411553cd508277cd4c83c6b058a7dda495
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-02-06 19:49:15 +0530

    Update 442_Find_All_Duplicates_in_an_Array.js

--- 442_Find_All_Duplicates_in_an_Array.js
+++ 442_Find_All_Duplicates_in_an_Array.js
@@ -1,13 +1,11 @@
 var findDuplicates = function(nums) {
-    let map = new Map()
     let ans = []
+    let t = 0
     for(let i = 0; i < nums.length; i++){
-        if(!map.has(nums[i])){
-            map.set(nums[i],1)
-        }
-        else{
-            ans.push(nums[i])
-        }
+        if(nums[i] < 0) t = Math.abs(nums[i]) - 1
+        else t = nums[i] - 1
+        if(nums[t] > 0) nums[t] = -nums[t]
+        else ans.push(Math.abs(nums[i]))
     }
     return ans
 };
