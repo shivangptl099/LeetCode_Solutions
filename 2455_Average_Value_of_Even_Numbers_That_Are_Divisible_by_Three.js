e7a53dcd6df3f40d0eba1fb2aa11280f780f812a
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-02-12 08:41:48 +0530

    Create 2455_Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.js

---
+++ 2455_Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.js
@@ -0,0 +1,12 @@
+var averageValue = function(nums) {
+    let sum = 0
+    let count = 0
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i] % 6 == 0){
+            sum += nums[i]
+            count++
+        }
+    }
+    if(count == 0) return 0
+    return Math.floor(sum / (count))
+};
