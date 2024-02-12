98cfd0d1eb2b8d55887ed83a81b25940534c4e9b
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-02-12 19:25:28 +0530

    Create 2828_Check_if_a_String_Is_an_Acronym_of_Words.js

---
+++ 2828_Check_if_a_String_Is_an_Acronym_of_Words.js
@@ -0,0 +1,11 @@
+var isAcronym = function(words, s) {
+    if(words.length != s.length)
+    {
+        return false;
+    }
+    for(let i = 0; i < words.length; i++)
+    {
+        if(words[i][0] != s[i]) return false;
+    }
+    return true;
+};
