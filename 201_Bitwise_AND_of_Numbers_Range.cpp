50d6ef860aaefa7d5cb4fdcec8cbea690a91c959
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-02-21 14:22:30 +0530

    Update 201_Bitwise_AND_of_Numbers_Range.cpp

--- 201_Bitwise_AND_of_Numbers_Range.cpp
+++ 201_Bitwise_AND_of_Numbers_Range.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        if(left == 1073741824 && right == 2147483647) return left;
-        long long l = left;
-        long long n = left;
-        l += 1;
-        while(l <= right){
-            if(n == 0) return 0;
-            n = n & l;
-            if(n == 0) return 0;
-            l++;
+        int rightShift = 0;
+        while(left != right && left < right){
+            rightShift++;
+            left = left >> 1;
+            right = right >> 1;
         }
-        return n;
+        for(int i = 0; i < rightShift; i++){
+            left = left << 1;
+        }
+        return left;
     }
 };
