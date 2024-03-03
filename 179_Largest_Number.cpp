51c8ebc1ba3055900fb4dcf36b4e65adb4bdd2ed
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-03-03 16:04:08 +0530

    Create 179_Largest_Number.cpp

---
+++ 179_Largest_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> sorted;
+        for(int x : nums){
+            sorted.push_back(to_string(x));
+        }
+        sort(sorted.begin(), sorted.end(), [](string a, string b){
+            return (a + b > b + a);
+        });
+        string ans;
+        for(string s : sorted){
+            ans += s;
+        }
+        if(ans[0] == '0') return "0";
+        return ans;
+    }
+};
