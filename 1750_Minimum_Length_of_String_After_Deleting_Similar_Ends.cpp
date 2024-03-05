235c03d7b6de8acc84f6c8b4a552782f004eaee4
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-03-05 09:49:48 +0530

    Update 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cpp

--- 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cpp
+++ 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends.cpp
@@ -1,3 +1,4 @@
+// My Solution 26ms
 class Solution {
 public:
     int minimumLength(string s) {
@@ -11,6 +12,21 @@ public:
             while(s[k] == c) k--;
             i = j;
             e = k;
+        }
+        return e - i + 1;
+    }
+};
+
+
+// Optimized Solution 19ms
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i = 0,e = s.length() - 1;
+        while(i < e && s[i] == s[e]){
+            char c = s[i];
+            while(i <= e && s[i] == c) i++;
+            while(i <= e && s[e] == c) e--;
         }
         return e - i + 1;
     }
