429e2fc54173177783b3019a86db6659e9751597
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-04-12 20:07:21 +0530

    Update 42_Trapping_Rain_Water.cpp

--- 42_Trapping_Rain_Water.cpp
+++ 42_Trapping_Rain_Water.cpp
@@ -5,19 +5,14 @@ public:
         vector<int> left(len);
         vector<int> right(len);
 
-        int max1 = height[0];
         left[0] = height[0];
-        for(int i = 1; i < len; i++){
-            max1 = max(height[i], max1);
-            left[i] = max1;
-        }
+        for(int i = 1; i < len; i++)
+            left[i] = max(height[i], left[i - 1]);
 
-        max1 = height[len - 1];
         right[len - 1] = height[len - 1];
-        for(int i = len - 2; i >= 0; i--){
-            max1 = max(height[i], max1);
-            right[i] = max1;
-        }
+        for(int i = len - 2; i >= 0; i--)
+            right[i] = max(height[i], right[i + 1]);
+
         int ans = 0;
         for(int i = 0; i < len; i++)
             ans += min(left[i], right[i]) - height[i];
