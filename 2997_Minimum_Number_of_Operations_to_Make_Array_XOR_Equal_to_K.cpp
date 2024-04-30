2c0b72123d13c578b37103edac35679cbb9097e0
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-04-30 23:02:26 +0530

    Update 2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.cpp

--- 2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.cpp
+++ 2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.cpp
@@ -4,8 +4,8 @@ public:
         int ans = 0;
         for(int i = 0; i <= 20; i++){
             int xorVal = 0;
-            for(int x : nums){
-                int bitVal = (1<<i) & x;
+            for(int element : nums){
+                int bitVal = (1<<i) & element;
                 xorVal ^= bitVal;
             }
             int kBitVal = (1<<i) & k;
