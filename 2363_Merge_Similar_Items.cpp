0875849c22781578e4f9035638640e048dc1d7d2
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-06-07 20:41:47 +0530

    Create 2363_Merge_Similar_Items.cpp

---
+++ 2363_Merge_Similar_Items.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int, int> mp;
+        for(int i = 0; i < items1.size(); i++)
+            mp[items1[i][0]] += items1[i][1];
+        for(int i = 0; i < items2.size(); i++)
+            mp[items2[i][0]] += items2[i][1];
+        vector<vector<int>> ans;
+        for(auto it : mp)
+            ans.push_back({it.first, it.second});
+        return ans;
+    }
+};
