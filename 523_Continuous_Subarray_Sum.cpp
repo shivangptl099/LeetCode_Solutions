44e7a7c5bfd90247d62a516a9467a806331ae3ef
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-06-08 23:59:59 +0530

    Update 523_Continuous_Subarray_Sum.cpp

--- 523_Continuous_Subarray_Sum.cpp
+++ 523_Continuous_Subarray_Sum.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        vector<int> arr(nums.size());
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
-            arr[i] = sum % k;
+        unordered_map<int, int> mp;
+        mp[0] = 0;
+        int n = nums.size(), sum = 0;
+        for(int i = 0; i < n; i++){
+            sum = (sum + nums[i]) % k;
+            if(mp.find(sum) != mp.end()){
+                if((i+1 - mp[sum] >= 2)) return true;
+            } else {
+                mp[sum] = i + 1;
+            }
         }
-        for(int i = 0; i < nums.size(); i++){
-            
-        }
+        return false;
     }
 };
