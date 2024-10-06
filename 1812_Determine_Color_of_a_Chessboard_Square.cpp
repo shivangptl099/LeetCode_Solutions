8532db2fef3854814962d4e698e311eb52b77e84
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-10-06 11:09:35 +0530

    Create 1812_Determine_Color_of_a_Chessboard_Square.cpp

---
+++ 1812_Determine_Color_of_a_Chessboard_Square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int x = coordinates[0] - 'a';
+        int y = coordinates[1];
+        if(x % 2 == 0)
+            if(y % 2 == 0) return true;
+            else return false;
+        else
+            if(y % 2 == 0) return false;
+            else return true;
+    }
+};
