f22c765ab0161b802172e697d3c5b53c609fbd54
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-11-06 12:14:34 +0530

    Create 3011_Find_if_Array_Can_Be_Sorted.cpp

---
+++ 3011_Find_if_Array_Can_Be_Sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < nums.size() - i; j++){
+                if(nums[j] <= nums[j + 1]) continue;
+                else {
+                    if(__builtin_popcount(nums[j]) == __builtin_popcount(nums[j + 1])){
+                        swap(nums[j], nums[j + 1]);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
