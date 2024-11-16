1e8bfe3adfd98f62e8d09de6989f49f39cc29378
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-11-16 14:10:39 +0530

    Create 3254_Find_the_Power_of_K_Size_Subarrays_I.cpp

---
+++ 3254_Find_the_Power_of_K_Size_Subarrays_I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq;
+        for(int i = 0; i < n; i++){
+            if(!dq.empty() && dq.front() <= i - k) dq.pop_front();
+            while(!dq.empty() && nums[dq.back()] != nums[i] - 1) dq.pop_back();
+            dq.push_back(i);
+            if(i >= k - 1){
+                if(dq.size() == k) ans.push_back(nums[dq.back()]);
+                else ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
