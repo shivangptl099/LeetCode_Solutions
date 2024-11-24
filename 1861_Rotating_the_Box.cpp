258600020f68e1a9b7eaa719a13ba39984351b2a
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-11-24 11:32:19 +0530

    Update and rename 1861_Rotating_the_Box.cpp_remain to 1861_Rotating_the_Box.cpp

--- 1861_Rotating_the_Box.cpp
+++ 1861_Rotating_the_Box.cpp
@@ -1,29 +1,32 @@
 class Solution {
-    void fill(vector<vector<char>>& res,int count,int x,int y){
+public:
+    void fill(vector<vector<char>>& ans, int count, int x, int y){
         while(count){
-            res[x][y]='#';
+            ans[x][y] = '#';
             x--;
             count--;
         }
     }
-public:
+
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size();
         int n = box[0].size();
-        vector<vector<char>> res(n, vector<char>(m, '.'));
+        vector<vector<char>> ans(n, vector<char>(m, '.'));
+
         for(int i = 0; i < m; i++){
             int count = 0;
             for(int j = 0; j < n; j++){
                 if(box[i][j] == '#') count++;
                 else if(box[i][j] == '*'){
-                    res[j][m - i - 1] = '*';
-                    fill(res, count, j - 1, m - i - 1);
+                    ans[j][m - i - 1] = '*';
+                    fill(ans, count, j - 1, m - i - 1);
                     count = 0;
                 }
             }
-            if(count > 0)
-                fill(res, count, n - 1, m - i - 1);
+            if(count > 0){
+                fill(ans, count, n - 1, m - i - 1);
+            }
         }
-        return res;
+        return ans;
     }
 };
