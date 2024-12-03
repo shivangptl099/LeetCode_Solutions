1d1b1338c712cb8b9b4128a80be1cc036a65b23b
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-12-03 10:57:25 +0530

    Update 2109_Adding_Spaces_to_a_String.cpp

--- 2109_Adding_Spaces_to_a_String.cpp
+++ 2109_Adding_Spaces_to_a_String.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        string ans,s1;
-        int i = 0,j = 0;
-        while(i < s.length()){
-            if(j < spaces.size() && i == spaces[j]){
+        string ans;
+        int j = 0, n = spaces.size();
+        for(int i = 0; i < s.length(); i++){
+            if(j < n && i == spaces[j]){
                 ans += " ";
                 j++;
             }
             ans += s[i];
-            i++;
         }
         return ans;
     }
