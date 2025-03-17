25c0edfa81597c7f0fc984261930338b6ee9192b
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2025-03-17 21:06:57 +0530

    Create 2206_Divide_Array_Into_Equal_Pairs.cpp

---
+++ 2206_Divide_Array_Into_Equal_Pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int x : nums){
+            mp[x]++;
+        }
+        for(auto it : mp){
+            if(it.second & 1) return false;
+        }
+        return true;
+    }
+};
